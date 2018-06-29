42d7222646c48a96e54ef9ba9eb05fe3c56ed0bb
Nico de Haen <mail@ndh-websolutions.de>
2018-06-29 09:55:30 +0200

    Allow access for user & group

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+
 if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         'TYPO3.CMS.IndexedSearch',
@@ -11,12 +12,13 @@ if (TYPO3_MODE === 'BE') {
             'Administration' => 'index,pages,externalDocuments,statistic,statisticDetails,deleteIndexedItem,saveStopwordsKeywords,wordDetail',
         ],
         [
-            'access' => 'admin',
+            'access' => 'user,group',
             'icon'   => 'EXT:indexed_search/Resources/Public/Icons/module-indexed_search.svg',
             'labels' => 'LLL:EXT:indexed_search/Resources/Private/Language/locallang_mod.xlf',
         ]
     );
 }
+
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('index_config');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('index_config', 'EXT:indexed_search/Resources/Private/Language/locallang_csh_indexcfg.xlf');
